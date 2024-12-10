5a527a306fa7a1aab7e6f166a0fc670691290248
shcerquera <180781585+shcerquera@users.noreply.github.com>
2024-12-10 12:46:28 +0100

    entornos

---
+++ src/Ejemplo.java
@@ -0,0 +1,13 @@
+public class Ejemplo {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+
+            System.out.println(i);
+
+        }
+
+        System.out.println("hola");
+
+    }
+}
